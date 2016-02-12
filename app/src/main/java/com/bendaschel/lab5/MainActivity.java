56df09ee36dc78bca9e9c082bc3d0295fb4c8332
Benjamin Daschel <coffeemaxed@gmail.com>
2016-02-11 20:31:36 -0800

    create basic two-layout MainActivity + tests

--- app/src/main/java/com/bendaschel/lab5/MainActivity.java
+++ app/src/main/java/com/bendaschel/lab5/MainActivity.java
@@ -1,10 +1,10 @@
 package com.bendaschel.lab5;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewParent;
@@ -30,16 +30,38 @@ public class MainActivity extends AppCompatActivity {
         mFragmentManager = getSupportFragmentManager();
         int numFragments = mFragmentManager.getFragments() != null ?
                 mFragmentManager.getFragments().size() : 0;
-        Log.d(TAG, String.format("Num fragments %d",numFragments));
+        Log.d(TAG, String.format("Num fragments %d", numFragments));
         if (savedInstanceState == null && mFragmentContainer != null){
             Fragment fragment = new ButtonFragment();
             mFragmentManager.beginTransaction()
-                    .add(R.id.fragment_container, fragment, ButtonFragment.TAG)
+                    .add(R.id.fragment_container, fragment, getString(R.string.tag_button_fragment))
                     .commit();
         }
     }
 
     public void onClick(View v){
         Log.d(TAG, "Button clicked");
+        updateTime();
+    }
+
+    private void updateTime() {
+        DetailFragment detailFragment = (DetailFragment) mFragmentManager
+                .findFragmentByTag(getString(R.string.tag_detail_fragment));
+
+        if (detailFragment == null) {
+            long currentTime = System.currentTimeMillis();
+            detailFragment = new DetailFragment();
+            Bundle args = new Bundle();
+            args.putString(DetailFragment.ARG_STRING_DISPLAY_TEXT, String.format("%d", currentTime));
+            detailFragment.setArguments(args);
+            mFragmentManager.beginTransaction()
+                    .replace(
+                            R.id.fragment_container,
+                            detailFragment,
+                            getString(R.string.tag_detail_fragment)
+                    )
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
--- app/src/test/java/com/bendaschel/lab5/MainActivityTest.java
+++ app/src/test/java/com/bendaschel/lab5/MainActivityTest.java
@@ -1,8 +1,9 @@
 package com.bendaschel.lab5;
 
-import android.content.pm.ActivityInfo;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,10 +13,12 @@ import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.util.ActivityController;
 
-import java_cup.Main;
-
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
 
@@ -34,8 +37,10 @@ public class MainActivityTest {
     public void testOnCreateNormal() throws Exception {
         MainActivity mainActivity = mActivityController.create().get();
         FragmentManager fragmentManager = mainActivity.getSupportFragmentManager();
-        Fragment buttonFrag = fragmentManager.findFragmentByTag(ButtonFragment.TAG);
-        Fragment detailFragment = fragmentManager.findFragmentByTag(DetailFragment.TAG);
+        Fragment buttonFrag = fragmentManager
+                .findFragmentByTag(mainActivity.getString(R.string.tag_button_fragment));
+        Fragment detailFragment = fragmentManager
+                .findFragmentByTag(mainActivity.getString(R.string.tag_detail_fragment));
         assertThat(buttonFrag, instanceOf(ButtonFragment.class));
         assertThat(detailFragment, nullValue());
     }
@@ -45,9 +50,24 @@ public class MainActivityTest {
     public void testOnCreateInLandscape() throws Exception {
         MainActivity mainActivity = mActivityController.create().get();
         FragmentManager fragmentManager = mainActivity.getSupportFragmentManager();
-        Fragment buttonFrag = fragmentManager.findFragmentByTag(ButtonFragment.TAG);
-        Fragment detailFragment = fragmentManager.findFragmentByTag(DetailFragment.TAG);
+        Fragment buttonFrag = fragmentManager
+                .findFragmentByTag(mainActivity.getString(R.string.tag_button_fragment));
+        Fragment detailFragment = fragmentManager
+                .findFragmentByTag(mainActivity.getString(R.string.tag_detail_fragment));
         assertThat(buttonFrag, instanceOf(ButtonFragment.class));
         assertThat(detailFragment, instanceOf(DetailFragment.class));
+    }
+
+    @Test
+    public void testButtonClickNormal() throws Exception {
+        MainActivity mainActivity = mActivityController.create().start().get();
+        View button = mainActivity.findViewById(R.id.btn_clickme);
+        long startTime = System.currentTimeMillis();
+        button.performClick();
+        long endTime = System.currentTimeMillis();
+        TextView timeTextView = (TextView) mainActivity.findViewById(R.id.tv_time);
+        assertThat(timeTextView, notNullValue());
+        long displayTime = Long.parseLong(timeTextView.getText().toString());
+        assertThat(displayTime, allOf(greaterThan(startTime), lessThan(endTime)));
     }
 }
